75fbc0ab160899a14f2d3b04e3aaa9f6dbd8cf80
JuanchiRV <jrevello@correo.um.edu.uy>
2024-06-07 18:52:01 -0300

    Unsetenv Juanchi

---
+++ nsilva3/builtin_unsetenv.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <error.h>
+#include <errno.h>
+#include "minish.h"
+
+int
+builtin_unsetenv(int argc, char **argv)
+{
+    if(argc==1){ //Si hay solo un argumento (el nombre del comando), ejecuta el siguiente bloque.
+        fprintf(stderr, "Argumentos insuficientes.\n"); //Imprime el mensaje "Argumentos insuficientes.\n" en la salida estándar de error (stderr).
+        return EXIT_FAILURE; //Devuelve EXIT_FAILURE (1) indicando una salida con error.
+    }
+    for(int i = 1; i<=argc; i++){
+        unsetenv(argv[i]); //Llama a la función unsetenv para eliminar la variable de entorno especificada en argv[i].
+    }
+    return EXIT_SUCCESS; //Devuelve EXIT_SUCCESS (0) indicando una salida exitosa.
+}
