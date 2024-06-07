1ecb8629f3f0f10ff2b51f07a8ae5977821eb9c7
Gaspar2004 <131988074+Gaspar2004@users.noreply.github.com>
2024-06-07 18:57:50 -0300

    Builtin_pid Gaspi

---
+++ builtin_pid.c
@@ -0,0 +1,16 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int builtin_pid (int argc, char **argv){
+    UNUSED(argc);//Se declara argc como no utilizado para evitar un warning durante compilacion.
+    UNUSED(argv);
+    printf("Process id: %d\n", getpid());//Imprime el id del proceso actual.
+    return 0;//Devuelve 0 indicando una salida exitosa.
+
+    }
+
+
+
+
