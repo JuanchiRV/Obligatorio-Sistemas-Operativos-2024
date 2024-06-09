31512bf0e8417eed7b2407f4335101f59b71ccf0
JuanchiRV <jrevello@correo.um.edu.uy>
2024-06-09 20:00:30 -0300

    status y unsetenv juanchi

--- nsilva3/Builtins/builtin_unsetenv.c
+++ nsilva3/Builtins/builtin_unsetenv.c
@@ -8,7 +8,7 @@ int
 builtin_unsetenv(int argc, char **argv)
 {
     if(argc==1){ //Si hay solo un argumento (el nombre del comando), ejecuta el siguiente bloque.
-        fprintf(stderr, "Argumentos insuficientes.\n"); //Imprime el mensaje "Argumentos insuficientes.\n" en la salida estándar de error (stderr).
+        fprintf(stderr, "ERROR, los argumentos son insuficientes.\n"); //Imprime el mensaje en la salida estándar de error (stderr).
         return EXIT_FAILURE; //Devuelve EXIT_FAILURE (1) indicando una salida con error.
     }
     for(int i = 1; i<=argc; i++){
